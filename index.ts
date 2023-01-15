9d7a896bf3c4b45fc3ccf360da63437abbcec3a5
fa34 <63210545+fa34@users.noreply.github.com>
2023-01-15 05:54:02 -0800

    Completion
    Project Completed

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+type answerType = {
+    userGuess:  "number"
+}
+
+const systemGeneratedNo =  Math.floor( Math.random() * 10) ;
+
+const answer : answerType = await inquirer.prompt([{
+
+    type: "number",
+    name:"userGuess",
+    message:"Enter your guess b/w 1 to 10"
+}]);
+
+const {userGuess} = answer ;
+console.log(userGuess ,"userGuess" , systemGeneratedNo , "SysNo")
+if(userGuess===systemGeneratedNo){
+    console.log("yaaaa your answer is correct \n you win")
+} else{
+    console.log("Try Better Luck Next Time")
+}
